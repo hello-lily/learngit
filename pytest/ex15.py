eb0efefd592d4ad1a6c21a2320a74344bbc5383b
lily <aliciali@139.com>
2017-04-22 16:30:54 +0800

    add ex14~16 about file op

---
+++ pytest/ex15.py
@@ -0,0 +1,21 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print ("Here's your file %r: " % filename)
+print (txt.read())
+txt.close()
+
+print ("Type the filename again: ")
+file_again = input(">")
+txt_again = open(file_again)
+
+print (txt_again.read())
+txt_again.close()
+
+#add close() function!  
+#from sys import argv:  
+#sys is a package, 
+#import argv means get the argv feature from that package. 
